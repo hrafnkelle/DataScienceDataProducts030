0b9dc88dba9b309b41e8b3cdb6a0fe43e6fe7b1f
Hrafnkell Eiríksson <he@klaki.net>
2015-07-17 22:17:16 +0000

    Improved layout and added explanations

--- server.R
+++ server.R
@@ -1,20 +1,30 @@
 
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-# 
-# http://www.rstudio.com/shiny/
-#
-
 library(shiny)
 library(ggplot2)
-
+library(scales)
 shinyServer(function(input, output) {
+  numTrials<-10000
+  numPiecesPerTrial<-50
+  
+  trials<-reactive({
+    t<-rbinom(numTrials,numPiecesPerTrial, input$prob/100)
+    data.frame(trials=t, meet_specs=(t>=(numPiecesPerTrial-1)))
+  })
    
   output$histogramPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    trials    <- data.frame(x=rbinom(100000,50,input$prob/100))
-    qplot(trials$x, weight=1/1000, ylab="Percentage of times found [%]", xlab="Count found", binwidth=0.5,ylim=c(0,100))
+    ggplot(data=trials(),
+           aes(x=trials,fill=meet_specs))+
+      geom_bar(aes(y = (..count..)/sum(..count..)), binwidth = 0.5,origin=-0.5)+
+      ylab("Ratio of trials [%]") + 
+      xlab("Count of rejects in trial") + 
+      scale_fill_discrete("Claim about trial was valid")+
+      scale_y_continuous(labels=percent) + 
+      scale_x_continuous(limits = c(numPiecesPerTrial-10, numPiecesPerTrial+1), breaks=seq(numPiecesPerTrial-10, numPiecesPerTrial))
+  })
+  
+  output$successratio <- renderText({
+    t<-trials()
+    sprintf('The salesman was right %.2f %% of the time.', 100*sum(t$meet_specs)/numTrials)
   })
   
 })
--- ui.R
+++ ui.R
@@ -1,29 +1,28 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-# 
-# http://www.rstudio.com/shiny/
-#
-
 library(shiny)
 
-shinyUI(pageWithSidebar(
+shinyUI(fluidPage(
   
-  # Application title
-  headerPanel("Demistifying Metal detector specs"),
-  
-  # Sidebar with a slider input for number of bins
+  headerPanel("Demistifying Metal detector specifications"),
+  p('A salesman tells you that his metal detector specifications will only miss one or less out of 50 test pieces with metal. The salesman was trying to make a 98% probability of detection understandable to the customer. Naturally the customer wants to validate this claim.'),
+  p(strong('Is a trial with 50 pieces a good way to test a metal detector performance, assuming all have a metal contaminant?')),
+  p('This page allows you to explore what the underlying probability of detection needs to be so that the salesmans claim is valid.'),
+  mainPanel(
+    h2('How often was the the claim of the salesman true?'),    
+    p('Simulating the trial 10000 times and counting how often the claim was true is a simple way to check how good the claim is.'),
+    textOutput("successratio"),
+    plotOutput("histogramPlot"),
+    p('The plot above shows the percentage of 50 piece trials that reject 50 pieces, 49 pieces, 48 pieces ...'),
+    p('The blue columns show the trials when the statement of the salesman was correct, the red when he was wrong')
+  ),
   sidebarPanel(
+    p('Adjust the detection probability to explore its effect on possible trial outcomes.'),
     sliderInput("prob",
-                "Probability of success:",
-                min = 50,
+                "Probability of metal detection:",
+                min = 95,
                 max = 100,
                 value = 98,
-                step=0.25)
-  ),
-  
-  # Show a plot of the generated distribution
-  mainPanel(
-    plotOutput("histogramPlot")
+                step=0.1),
+    p('Each column in the plot above shows the percentage of trials with the same number of succesful outcomes.'),
+    p('We simulate 10000 trials with 50 pieces for you too see what can happen.')
   )
 ))
